c765fd5785b1c72aedb1f2713f8a10d7163ad58d
Macovei Catalin <macovei.catalin91@gmail.com>
2020-11-05 09:01:53 +0200

    added car class; studied variables

---
+++ src/main/java/org/fasttrackit/Car.java
@@ -0,0 +1,16 @@
+package org.fasttrackit;
+
+public class Car {
+    // instance variable
+    String name;
+    double fuelLevel;
+    double mileage;
+    double totalTravelDistance;
+    double maxSpeed;
+
+
+    int doorCount;
+    boolean damaged;
+    String color;
+
+}
